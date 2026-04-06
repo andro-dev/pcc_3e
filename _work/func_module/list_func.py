d70939e23a7c6264f31d8a5357bb7db411a487f2
Androdev Rhel <andodev.rhel@gmail.com>
2026-04-06 14:23:42 -0400

    add chapter 9 files

--- _work/func_module/list_func.py
+++
@@ -1,9 +0,0 @@
-
-
-def create_list_of_random_numbers(number_of_items: int, start:int, stop:int):
-    import random
-    return [random.randint(start, stop) for n in range(number_of_items)]
-
-
-print(create_list_of_random_numbers(5, 100, 200))
-print(create_list_of_random_numbers(10, 100, 1000))
